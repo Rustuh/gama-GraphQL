3dc36ed5cd34ae391c6522ea7cfea3885fdf0b9b
Rustuh <lipefel@gmail.com>
2021-07-20 13:43:42 -0300

    Aula 03 Pacote com React.js

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile }  from 'fs';
-import { resolve } from 'path';
 import { parse } from 'querystring';
 
 const server = createServer((request, response) => {
@@ -18,38 +16,6 @@ const server = createServer((request, response) => {
             break
         }
 
-         case '/sign-in': {
-            const path = resolve(__dirname, './pages/sign-in.html');
-            readFile(path, (error, file) => {
-                if (error) {
-                    response.writeHead(500, 'Cant process HTML file.');
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file);
-                response.end();
-            });
-            break
-        }
-        
-        case '/home': {
-            const path = resolve(__dirname, './pages/home.html');
-            readFile(path, (error, file) => {
-                if (error) {
-                    response.writeHead(500, 'Cant process HTML file.');
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file);
-                response.end();
-            });
-            break
-        }
-
         case '/authenticate': {
             let data = '';
             request.on('data', (chunk) => {
@@ -58,9 +24,6 @@ const server = createServer((request, response) => {
 
             request.on('end', () => {
                 const params = parse(data);
-                response.writeHead(301, {
-                    Location: '/home',
-                });
                 response.end();
             });
             
