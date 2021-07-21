4fe7f373395b71da890f4e279ec49d7593b3833d
Rustuh <lipefel@gmail.com>
2021-07-20 23:33:32 -0300

    Aula 05 Requisição assincrona

--- packages/web/src/Router.js
+++ packages/web/src/Router.js
@@ -1,4 +1,3 @@
-import { func } from 'prop-types';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -9,7 +8,7 @@ export default function Router() {
     return (
         <Switch>
             <Route exact path={('', '/')} component={Home} />
-            <Route exact path='sign-in' component={SignIn} />
+            <Route exact path='/sign-in' component={SignIn} />
         </Switch>
     )
 }
